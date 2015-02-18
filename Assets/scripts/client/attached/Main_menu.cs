3ccd1021d7bb31b5f9c87584caee46c2cc03958d
XD-face-me <Bastiaan97.bvh@gmail.com>
2015-02-18 14:49:02 +0100

    There is a server now. It doesn't do anything, but it works.

--- Assets/scripts/client/attached/Main_menu.cs
+++ Assets/scripts/client/attached/Main_menu.cs
@@ -5,10 +5,50 @@ namespace Assets.scripts.client.attached
 {
     public class Main_menu : MonoBehaviour
     {
-
+        public int disconnect_timeout;
+        HostData[] hostdata;
+        public UnityEngine.UI.Text servers;
         public void start_game()
         {
             Application.LoadLevel("world");
+        }
+
+        public void connect()
+        {
+            Network.Connect(hostdata[0]);
+        }
+
+        public void disconnect()
+        {
+            Network.Disconnect(disconnect_timeout);
+        }
+        public void refresh()
+        {
+            Debug.Log("requested hostlist");
+            MasterServer.RequestHostList("BassieTBTBS");
+        }
+
+        void Awake()
+        {
+            refresh();
+
+        }
+
+        void Start()
+        {
+            hostdata = MasterServer.PollHostList();
+        }
+        void Update()
+        {
+            if (hostdata.Length == 0)
+            {
+                Debug.Log("searching for a server");
+                hostdata = MasterServer.PollHostList();
+            }
+            foreach(HostData element in hostdata)
+            {
+                servers.text=(element.gameName + " " + element.connectedPlayers + " / " + element.playerLimit);
+            }
         }
 
     }
--- Assets/scripts/server/Server.cs
+++ Assets/scripts/server/Server.cs
@@ -8,26 +8,96 @@ namespace Assets.scripts.client
 {
     class Server : MonoBehaviour
     {
+        #region
         enum Messagetype
         {
             error,
             warning,
             information,
         }
+        struct client
+        {
+            public string ip;
+        }
+
         public int players;
         public int port;
+        public int disconnect_timeout;
         public UnityEngine.UI.Text connected_clients;
         public UnityEngine.UI.Text log;
-        public UnityEngine.UI.Button start_button;
-        public void start_stop_server()
+        public UnityEngine.UI.InputField server_name_input;
+        public UnityEngine.UI.Text server_name;
+        public UnityEngine.UI.Text start_button_text;
+        private string name;
+        private List<client> clients = new List<client>();
+        #endregion
+
+
+        private void start_server()
         {
-            if(!Network.isServer)
+            if (server_name.text != "")
             {
+                name = server_name.text;
                 Network.InitializeServer(players, port, !Network.HavePublicAddress());
-                start_button.interactable = false;
+                start_button_text.text = "Stop Server";
+                print_log("starting server with the name: " + name);
+                MasterServer.RegisterHost("BassieTBTBS", name, "Still needs a good name");
+                server_name_input.interactable = false;
+            }
+            else
+            {
+                print_log("Please input a server name", Messagetype.error);
             }
         }
 
+        private void print_log(String message, Messagetype message_type = Messagetype.information)
+        {
+            switch (message_type)
+            {
+                case Messagetype.information:
+                    log.text += "Info: " + message + "\n";
+                    break;
+                case Messagetype.error:
+                    log.text += "<color=red>Error: " + message + "</color>\n";
+                    break;
+                case Messagetype.warning:
+                    log.text += "<color=#FFFF5C>Warning: " + message + "</color>\n";
+                    break;
+            }
+        }
+        private void stop_server()
+        {
+            Network.Disconnect(disconnect_timeout);
+            start_button_text.text = "Start Server";
+            print_log("Stopped the server");
+            server_name_input.interactable = true;
+        }
+
+        //Gui calls
+        #region MyRegion
+        public void start_stop_server()
+        {
+            if (!Network.isServer)
+            {
+                start_server();
+            }
+            else
+            {
+                stop_server();
+            }
+        } 
+        #endregion
+        //UnityEngine calls
+        #region
+        void OnGUI()
+        {
+            connected_clients.text = "";
+            foreach (client client in clients)
+            {
+                connected_clients.text += client.ip + "\n";
+            }
+            
+        }
         void OnServerInitialized()
         {
             print_log("Server initialized and ready");
@@ -36,22 +106,18 @@ namespace Assets.scripts.client
         void OnPlayerConnected(NetworkPlayer player)
         {
             print_log("Player connected from " + player.ipAddress + ":" + player.port);
+            client client = new client();
+            client.ip = player.ipAddress;
+            clients.Add(client);
         }
 
-        private void print_log(String message, Messagetype message_type = Messagetype.information)
+        void OnPlayerDisconnected(NetworkPlayer player)
         {
-            switch (message_type)
-            {
-                case Messagetype.information:
-                    log.text += "info:" + message + "\n";
-                    break;
-                case Messagetype.error:
-                    log.text += "<color=red>Info: " + message + "</color>\n";
-                    break;
-                case Messagetype.warning:
-                    log.text += "<color=#FFFF5C>Info: " + message + "</color>\n";
-                    break;
-            }
+            print_log("Player disconnected from " + player.ipAddress + ":" + player.port);
+            int index = clients.FindIndex(_client => _client.ip == player.ipAddress);
+            clients.RemoveAt(index);
         }
+        #endregion
+        
     }
 }
